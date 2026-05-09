dbbb9dea9c98428abe5413c23fc3747150dd53dc
arthuneas <arthursalmeidacontato@gmail.com>
2026-05-08 22:43:17 -0300

    :)

--- MuxDemux/UDP/clienteUDP.py
+++ MuxDemux/UDP/clienteUDP.py
@@ -6,29 +6,31 @@ port = 12345
 
 nickname = input("Choose a nickname: ")
 
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #declarar um socket de internet e um objeto TCP
+client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) #declarar um socket de internet e um objeto UDP
 client.connect((host, port)) #conectando o cliente ao servidor
+
+mensagem_entrada = f"JOIN:{nickname}"
+client.send(mensagem_entrada.encode('ascii'))
 
 def receive():
     while True:
         try:
             message = client.recv(1024).decode('ascii')
-            
-            if message == 'NICK':
-                client.send(nickname.encode('ascii'))
-            else: 
-                print(message)
+            print(message)
                 
-        except:
-            print("an erroer occurred!")
+        except Exception as e:
+            print(f"ocorreu um erro: {e}")
             client.close()
             break
             
             
 def write():
     while True:
-        message = f'{nickname}: {input("")}'
-        client.send(message.encode('ascii'))
+        try:
+            message = f'{nickname}: {input("")}'
+            client.send(message.encode('ascii'))
+        except:
+            break
         
 receive_thread = threading.Thread(target=receive)
 receive_thread.start()
